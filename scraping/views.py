7a7af795d26838e2c055730d684917023d773191
Finoana <finoanaandriatsilavoo@gmail.com>
2020-11-26 18:47:12 +0300

    load spin and nav page

--- scraping/views.py
+++ scraping/views.py
@@ -17,44 +17,58 @@ app.config.from_object('config')
 list_entreprise_gl = []
 list_doc_gl = []
 siren_gl = ''
-driver = None
+driver_gl = None
 newChoice_gl = False
 rs_sociale_gl = ''
 period_gl = None
 siren_choice_gl = None
+list_services_gl = []
+serviceName_gl = None
 
 @app.route('/home')
 def index():
     # initialise driver
-    print(app.instance_path)
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    print(basedir)
-    global driver
-    driver = chrome(basedir).driver()
-    try:
-        login = NetLog(driver).run_login(app.config['URL_PAGE'], app.config['USER_FIRST_NAME'],
-                                         app.config['USER_LAST_NAME'], app.config['SIRET'], app.config['PASSWORD'])
-
-    except ElementNotInteractableException as log_error:
-        print('misy erreur')
-        print(log_error)
-        print(url_for('index'))
-
-        return redirect(url_for('index'))
-
+    global driver_gl
+    global list_services_gl
     list_services = []
-    if login:
-        list_services = NetLog(driver).list_services()
+    if driver_gl == None:
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        driver = chrome(basedir).driver()
+        driver_gl = driver
+        try:
+            login = NetLog(driver).run_login(app.config['URL_PAGE'], app.config['USER_FIRST_NAME'],
+                                            app.config['USER_LAST_NAME'], app.config['SIRET'], app.config['PASSWORD'])
+
+        except ElementNotInteractableException as log_error:
+            print('misy erreur')
+            print(log_error)
+            print(url_for('index'))
+
+            return redirect(url_for('index'))
+        if login:
+            list_services = NetLog(driver).list_services()
+            list_services_gl = list_services
+    elif driver_gl != None and len(list_services_gl) != 0 :
+        list_services = list_services_gl
+        driver_gl.get('https://portail.net-entreprises.fr/priv/declarations')
+
     return render_template('dashboard.html', listServices=list_services, services=True)
 
 
 @app.route('/operation/<serviceName>')
 def listeServices(serviceName):
-    list_entreprise = NetLog(driver).list_entreprise(serviceName)
-    if len(list_entreprise) != 0:
-        global list_entreprise_gl
-        list_entreprise_gl = list_entreprise
-    return render_template('operation.html', listEntreprise=list_entreprise, listeDoc=0)
+    global serviceName_gl
+    if serviceName_gl == None:
+        serviceName_gl =  serviceName
+        list_entreprise = NetLog(driver_gl).list_entreprise(serviceName)
+        if len(list_entreprise) != 0:
+            global list_entreprise_gl
+            list_entreprise_gl = list_entreprise
+    else:
+        list_entreprise = list_entreprise_gl
+        driver_gl.get('https://ducs.net-entreprises.fr/com.netducsi.client.htmlhome/servlets/AccrochageEFIEDIChoiceOut.do')
+        
+    return render_template('operation.html', listEntreprise=list_entreprise, listeDoc=0, serviceName = serviceName_gl)
 
 
 @app.route('/doc_urssaf')
@@ -69,7 +83,7 @@ def docUrssaf():
     siren_choice_gl =  request.args.get('sirenChoice', None)
     print('siren_choice_gl------------------------')
     print(siren_choice_gl)
-    list_doc = NetLog(driver).doc_urssaf(siren, newChoice_gl, siren_choice_gl)
+    list_doc = NetLog(driver_gl).doc_urssaf(siren, newChoice_gl, siren_choice_gl)
     # check new siren choice
     for list in list_doc:
         if 'newChoice' in list:
@@ -86,7 +100,7 @@ def docUrssaf():
             noDoc = existDoc
         else:
             noDoc = existDoc
-    return render_template('operation.html', listEntreprise=list_entreprise_gl, listeDoc=list_doc, noDoc=noDoc, newChoice=newChoice_gl, rs_sociale=rs_sociale_gl, siren_choice_gl=siren_choice_gl, last_siren=siren)
+    return render_template('operation.html', listEntreprise=list_entreprise_gl, listeDoc=list_doc, noDoc=noDoc, newChoice=newChoice_gl, rs_sociale=rs_sociale_gl, siren_choice_gl=siren_choice_gl, last_siren=siren, serviceName = serviceName_gl)
 
 
 @app.route('/to_urrsaf')
@@ -99,9 +113,9 @@ def toUrssaf():
         siren = siren_choice_gl
     else:
         siren = siren_gl
-    log_urssaf = NetLog(driver).to_urssaf(periode, siren, siren_choice)
+    log_urssaf = NetLog(driver_gl).to_urssaf(periode, siren, siren_choice)
     if log_urssaf:
-        return render_template('download.html', urssaf_doc=periode, rs_sociale=rs_sociale_gl)
+        return render_template('download.html', urssaf_doc=periode, rs_sociale=rs_sociale_gl, serviceName = serviceName_gl)
 
 
 @app.route('/download', methods=['GET', 'POST'])
@@ -111,18 +125,18 @@ def download():
     type = request.args['types']
     print(doc)
     print(type)
-    NetLog(driver).initialise_downloadFile(doc, type)
-    driver.get('https://ducs.net-entreprises.fr/com.netducsi.client.htmlhome/servlets/AccrochageEFIEDIChoiceOut.do')
-    return render_template('operation.html', listEntreprise=list_entreprise_gl, listeDoc=0)
+    NetLog(driver_gl).initialise_downloadFile(doc, type)
+    driver_gl.get('https://ducs.net-entreprises.fr/com.netducsi.client.htmlhome/servlets/AccrochageEFIEDIChoiceOut.do')
+    return render_template('operation.html', listEntreprise=list_entreprise_gl, listeDoc=0, serviceName = serviceName_gl, download = True)
 
 
 @app.route('/logout')
 def logout():
-    driver.quit()
+    driver_gl.quit()
     return 'déconnecter'
 
 
 @app.route('/retour_choix_entreprise')
 def to_choix():
-    NetLog(driver).to_choix()
-    return render_template('operation.html', listEntreprise=list_entreprise_gl)
+    NetLog(driver_gl).to_choix()
+    return render_template('operation.html', listEntreprise=list_entreprise_gl, serviceName = serviceName_gl)
