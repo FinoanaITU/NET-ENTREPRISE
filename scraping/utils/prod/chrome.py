5a3a40b375470d6126a61c64210b58caa832c564
Finoana <finoanaandriatsilavoo@gmail.com>
2020-12-03 18:06:35 +0300

    change path

--- scraping/utils/prod/chrome.py
+++ scraping/utils/prod/chrome.py
@@ -10,7 +10,7 @@ class chrome ():
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         # chrome_options.add_argument("--no-sandbox")
         print(self.basedir)
         if os.path.isdir(self.basedir):  
